948d89aa6c697f742d4c5afc25b6b294876be5b0
Pablo Speciale <spablo@fceia.unr.edu.ar>
2011-06-28 16:54:36 -0300

    Minimos cuadrados vs polimonios de Legendre

--- min_cuadrados_VS_legendre.m
+++ min_cuadrados_VS_legendre.m
@@ -1,4 +1,4 @@
-% f = u( [0:0.0001:1] )';
+f = u( [0:0.0001:1] )';
 x = f;
 N=10;
 Poli_Legendre = fliplr(legendre_coefficients_matrix(N));
@@ -11,8 +11,10 @@ end
 Lambdax=Lx\x;
 xest=Lx*Lambdax;
 
-figure(2)
+figure(1)
+subplot(121)
 plot( xx, xest, 'Color', 'green' );
+title( ['Minimos cuadrados'] );
 fmin = min(x);
 fmax = max(x);
 axis([0 1 fmin-fmin*0.01 fmax+fmax*0.01]);
@@ -20,4 +22,21 @@ hold on;
 plot( xx, x, 'Color', 'red');
 hold off;
 
-vpa(RMS( x, xest ))
+error_minimos_cuadrado = vpa(RMS( x, xest ))
+
+subplot(122)
+
+
+for n=7:25 
+    xest2 = aprox_discreta(n,x);
+    xest2 = xest2(1:end-1);
+    plot(xx, xest2)
+    title( ['N = ',int2str(n)] );
+    axis([0 1 fmin-fmin*0.01 fmax+fmax*0.01]);
+    hold on;
+    plot( xx, x, 'Color', 'red');
+    hold off;
+    error = vpa(RMS( x, xest2 ))
+    pause;
+end
+
