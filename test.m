f8f0a6fffbc01d6cc8246c8308251cac130f964d
Pablo Speciale <spablo@fceia.unr.edu.ar>
2011-07-08 18:01:30 -0300

    Store the feature calculation

--- test.m
+++ test.m
@@ -1,63 +1,23 @@
+%% methods
+global method;
+method = containers.Map;
+method('moments_L') = 1;
+method('least_square_L') = 2;
 
 
-%%
-i=1;
-x = db2{i}.channel{1};
-y = db2{i}.channel{2};
-curves_features = feature_extraction( x, y, 10 );
-
-
-%%
-d = 16;
-num = 9;
-global C;
-C = legendre_coefficients_matrix(d);
-db = feature_extraction_db( db, d );
-db_testing = feature_extraction_db( db_testing, d );
-db_splited_training1 = split(db);
-db_splited_testing = split(db_testing);
-
-
-% N = length(db_splited);
-N = 110;
-
-training = [];
-group = [];
-for label=1:10
-    for i=1:N
-        training = [ training; db_splited_training1{label,i}.features ];
-        group = [ group; mod(label,10) ];
-    end
-end
-
-%     group = [[1:10], [1:10]];
-
-testing = [];
-for i=1:N
-    testing = [ testing; db_splited_testing{num,i}.features ];
-end
-
-% for i=21:30
-%     r = randi([21 30],1);
-% %     testing = [ testing; db{ r } ];
-%     testing = [ testing; db{ i } ];
-% end
-
-class = knnclassify(testing, training, group);
-
-s = [d,num,length(class(class==num))/length(class)]
-
-
-%% test
-result = []
-for d=3:20
+%% Features extraction
+for d=3:25
     global C;
     C = legendre_coefficients_matrix(d);
 
-    % Features extraction
-%     db = feature_extraction_db(db, d);
+    db = feature_extraction_db(db, d);
+end
 
-    % cross-validation
+%% cross-validation
+m = 'least_square_L';
+result = []
+for d=3:25
+
     mat_labels = cell2mat(db.get_labels());
     mat_labels = mat_labels(mat_labels~='1');
 
@@ -77,20 +37,21 @@ for d=3:20
 
         % training
         for i=training_domain     
-            training = [ training; db.get_trace(label,i).features ];
+            training = [ training; db.get_trace(label,i).features{method(m),d} ];
             training_class = [ training_class; label ];
     %         [num2cell(label),i]
         end
 
         % testing
         for i=testing_domain
-            testing = [ testing; db.get_trace(label,i).features ];
+            testing = [ testing; db.get_trace(label,i).features{method(m),d} ];
             testing_class = [ testing_class; label ];
         end
     end
 
 
-    class = knnclassify(testing, training, training_class, 1, 'cityblock' );
+%     class = knnclassify(testing, training, training_class, 1, 'cityblock' );
+    class = knnclassify(testing, training, training_class, 1, 'euclidean' );
 %     class = classify(testing, training, training_class, 'mahalanobis' );
     recognition = 100 * sum(class == testing_class) / length(class);
     [d recognition]
@@ -100,26 +61,26 @@ end
 
 %%
 
-% result_minCuadrados = result
+% result_minCuadrados = result;
 % result_minCuadrados_cityblock = result;
 % result_minCuadrados_mahalanobis = result;
 
 
 % result_legendre = result
 % result_legendre_cityblock = result
-result_legendre_mahalanobis = result;
+% result_legendre_mahalanobis = result;
 
 
-% plot( result_minCuadrados(:,1), result_minCuadrados(:,2), '-o', 'MarkerFaceColor','b');
-
-plot( result_minCuadrados_cityblock(:,1), result_minCuadrados_cityblock(:,2), '-o', 'Color', 'blue', 'MarkerFaceColor','b');
+plot( result_minCuadrados(:,1), result_minCuadrados(:,2), '-o', 'MarkerFaceColor','b');
 hold on;
+plot( result_minCuadrados_cityblock(:,1), result_minCuadrados_cityblock(:,2), '-o', 'Color', 'cyan', 'MarkerFaceColor','b');
+% hold on;
 plot( result_minCuadrados_mahalanobis(:,1), result_minCuadrados_mahalanobis(:,2), '-o', 'Color', 'red', 'MarkerFaceColor','b');
 
 
 % plot( result_legendre(:,1), result_legendre(:,2), '-o', 'Color', 'green', 'MarkerFaceColor','g');
 % plot( result_legendre_cityblock(:,1), result_legendre_cityblock(:,2), '-o', 'Color', 'red', 'MarkerFaceColor','g');
-plot( result_legendre_mahalanobis(:,1), result_legendre_mahalanobis(:,2), '-o', 'Color', 'green', 'MarkerFaceColor','g');
+% plot( result_legendre_mahalanobis(:,1), result_legendre_mahalanobis(:,2), '-o', 'Color', 'green', 'MarkerFaceColor','g');
 hold off;
 
 
--- trace.m
+++ trace.m
@@ -1,13 +1,20 @@
 classdef trace
-   properties
-      channel   % channel. i.e. [ [x1 x2 .. xn], [y1 y2 ... yn] ]
-      label     % Ground truth
-      dim       % i.e [800,600]
-      features  % curves features, [x_alpha y_alpha]
-   end
+    properties
+        channel   % channel. i.e. [ [x1 x2 .. xn], [y1 y2 ... yn] ]
+        label     % Ground truth
+        dim       % i.e [800,600]
+        features  % features{method(m),d} = curves_features, [x_alpha y_alpha]
+    end
    
-%    methods
-%       methodName
-%    end
-   
+    methods       
+        % m: method
+        % d: degree
+        function obj = fe( obj, m, d )
+            global method;
+
+            obj.features{method(m),d} = feature_extraction( obj.channel{1}, ...
+                                                            obj.channel{2}, ...
+                                                            d );           
+        end
+    end
 end
