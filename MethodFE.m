6aae479da0663a624bb851ed4697b9099a350824
Pablo Speciale <spablo@fceia.unr.edu.ar>
2011-08-02 19:33:33 -0300

    Added Chebyshev method

--- MethodFE.m
+++ MethodFE.m
@@ -1,6 +1,8 @@
 classdef MethodFE < uint32
    enumeration
-      moments_L       (1)
+      moments_L       (1)   % Legendre
       least_square_L  (2)
+      moments_C       (3)   % Chebyshev
+      least_square_C  (4)   
    end
 end
