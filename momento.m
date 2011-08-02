4653965417d31a6082a9c895d7c1e6dd3b8372e6
Pablo Speciale <spablo@fceia.unr.edu.ar>
2011-08-02 19:33:10 -0300

    El "delta" dentro de la función (...^k) para evitar algunos errores de calculo

--- momento.m
+++ momento.m
@@ -13,10 +13,10 @@ end
 % delta=b/L;
 % muk=0;
 % for n=1:L-1
-%     muk=muk+( f(n)/2*(delta^(k+1))*(n-1)^k + n^k/2*(delta^(k+1))*f(n+1) );
+%     muk=muk+( f(n)/2*((n-1)*delta)^k*delta + (n*delta)^k/2*delta*f(n+1) );
 % end
-% 
-% 
+
+
 
 % delta=1/L;
 % muk=0;
