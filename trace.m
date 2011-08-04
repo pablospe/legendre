82fd9b7ca75931403449d7bc11eb0b564e46d9bb
Pablo Speciale <spablo@fceia.unr.edu.ar>
2011-08-03 23:17:25 -0300

    Added Normalization to Trace class

--- trace.m
+++ trace.m
@@ -1,12 +1,13 @@
 classdef trace < handle
     properties
-        channel   % channel. i.e. [ [x1 x2 .. xn], [y1 y2 ... yn] ]
-        label     % Ground truth
-        dim       % i.e [800,600]
+        channel      % channel. i.e. [ [x1 x2 .. xn], [y1 y2 ... yn] ]
+        label        % Ground truth
+        dim          % i.e [800,600]
+        normalized   % {true, false}
     end
    
     methods
-       %% Constructor
+        %% Constructor
         function obj = trace( x, y )
             if nargin < 1
                 return;
@@ -14,15 +15,41 @@ classdef trace < handle
             
             obj.channel{1} = x;
             obj.channel{2} = y;
+            obj.normalized = false;
         end
         
-       %% Feature Extraction
+        %% Normalization
+        function obj = normalize( obj )
+            if( isempty(obj.normalized) )
+                obj.normalized = false;
+            end
+            
+            if( obj.normalized == false )
+                x = obj.channel{1};
+                y = obj.channel{2};
+
+                obj.channel{1} = (x-min(x))/(max(x)-min(x));
+                obj.channel{2} = (y-min(y))/(max(y)-min(y));
+
+                obj.normalized = true;
+            end
+        end
+        
+        %% Feature Extraction
         % m: method
         % d: degree
-        function curves_features = feature_extraction( obj, m, d )
+        function curves_features = feature_extraction( obj, m, d, normalize )            
+            if ~exist('normalize', 'var')
+                normalize = false;
+            end
+            
+            if( normalize == true )
+                obj.normalize();
+            end
+            
             curves_features = feature_extraction( obj.channel{1}, ...
                                                   obj.channel{2}, ...
-                                                  m, d );           
-       end
+                                                  m, d );
+        end
     end
 end
